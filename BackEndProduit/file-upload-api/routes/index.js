71f39b70ae9c8c5b16d7f4c28d58114b14d4a0dd
Chris Ahona <chris.ahona@gmail.com>
2019-02-15 14:25:55 +0100

    BackEndProduit

--- BackEndProduit/file-upload-api/routes/index.js
+++ BackEndProduit/file-upload-api/routes/index.js
@@ -1,17 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mv = require('mv');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Application d\'upload'});
 });
 
 router.post('/upload', (req, res, next) => {
-  console.log('here')
-  console.log(req );
-  const fileName = req.files.file.name
-  console.log('filename',fileName);
-  
+  let fileUpload = req.files.file;
+  if (fileUpload.length == 0) {
+    return res.status(400).send('Pas de fichiers!');
+  }
+  console.log(fileUpload);
+  fileUpload.mv('./public/upload/TextTrackList.jpg', (err)=> {
+    if(err) {
+      return res.status(500).send(err);
+    } else {
+      res.send("uploadé!")
+    }
+  });
 })
 
 module.exports = router;
